a9a5f80c2092f927902fb53714a538839ca0e24b
Keshav Maiya <keshavrajmaiya@gmail.com>
2026-03-10 23:53:57 +0530

    Updated JS with Azure IP

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,7 +1,7 @@
 // js/api.js
 
 // Change this if your Django server runs on a different port
-const API_BASE_URL = 'http://127.0.0.1:8000/api'; 
+const API_BASE_URL = 'http://4.240.96.118:8000/api'; 
 
 export const api = {
     // 1. Auth Utilities

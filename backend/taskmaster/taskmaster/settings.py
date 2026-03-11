23969dc46fdb113afba63e50d355e3e7b880f51a
Keshav Maiya <keshavrajmaiya@gmail.com>
2026-03-11 23:55:23 +0530

    Add static volumes and admin routing to NGINX and docker-compose

--- backend/taskmaster/taskmaster/settings.py
+++ backend/taskmaster/taskmaster/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,6 +178,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # where the staticfiles goes on running collectstatic
 
 # ------------- CHANNEL & REDIS CONFIGURATION -----------
 
